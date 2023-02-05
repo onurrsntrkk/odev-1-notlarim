4b7c367796398a797d103e53f60dec63391e4d3f
onurrsntrkk <123196289+onurrsntrkk@users.noreply.github.com>
2023-02-05 18:57:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace toplama
+{
+    class Program
+    {
+        public static int b;
+        static void Main(string[] args)
+        {
+            int a = 10;
+            {
+                b = 20;
+            }
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+        }
+    }
+}
